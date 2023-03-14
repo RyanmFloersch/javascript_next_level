a1be1754f0c3dc1aa96b9c3b1c5e18f2428d6ba9
Ryan <ryanmfloersch@gmail.com>
2023-03-14 12:19:20 -0400

    Half way through the class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 
 
 var h1=document.querySelector('h1');
+var button = document.querySelector('button');
+var body = document.body;
 
 h1.innerText = 'JS Next Level'
+
+button.addEventListener('click', function(){
+    // var body = document.body;
+    // body.style.cssText = "background-color: #777; color: red" ;
+    body.setAttribute('class', 'darkmode');
+    
+})
