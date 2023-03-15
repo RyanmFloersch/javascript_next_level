7b98630cd690a8b1df066673c1f02ea60dc9bf97
Ryan <ryanmfloersch@gmail.com>
2023-03-15 10:17:28 -0400

    Adding the changes made

--- js/script.js
+++ js/script.js
@@ -1,14 +1,63 @@
 
 
-var h1=document.querySelector('h1');
+var h1 = document.querySelector('h1');
 var button = document.querySelector('button');
 var body = document.body;
 
-h1.innerText = 'JS Next Level'
+//Create a variable that trackes if dark mode is active
+var isDark = false;
+var h3 = document.querySelector("h3");
+var count = 5;
 
-button.addEventListener('click', function(){
-    // var body = document.body;
-    // body.style.cssText = "background-color: #777; color: red" ;
-    body.setAttribute('class', 'darkmode');
-    
-})
+
+function setHeaderText() {
+    h1.innerText = 'JS Next Level'
+
+}
+
+//Togle between light mode and dark
+//If varaible is true, switch to light else switch to dark 
+
+function toggleDarkMode() {
+    if (isDark === false) {
+        body.classList.add("darkmode");
+        button.innerText = 'Light Mode';
+        isDark = true;
+    } else {
+        body.classList.remove("darkmode");
+        button.innerText = 'Dark Mode';
+        isDark = false;
+    }
+
+}
+
+
+function colorBlast() {
+    body.classList.add('splash');
+}
+
+function countdown() {
+
+    h3.innerText = "count " + count;
+    var timer = setInterval(function () {
+        count--;
+        h3.innerText = "count " + count;
+
+        if (count === 0) {
+            clearInterval(timer);
+            // colorBlast();
+        }
+    }, 1000)
+}
+
+
+function init() {
+
+    setHeaderText();
+    countdown();
+
+    button.addEventListener('click', toggleDarkMode);
+
+}
+
+init();
